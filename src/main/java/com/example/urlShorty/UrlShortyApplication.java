3f13da80b37c86ebfbb240fe901d8cb182e5df14
Md Sameer <mdsameer8729@gmail.com>
2026-01-13 17:53:06 +0530

    config: add Redis configuration and expose StringRedisTemplate

---
+++ src/main/java/com/example/urlShorty/UrlShortyApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlShorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urlShorty/UrlShortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlShorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
